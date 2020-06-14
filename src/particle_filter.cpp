1f8d99ab9a6d18b4b1d2715f2cf6e9eacda025c0
salabson4@yahoo.co.uk <salabson4@yahoo.co.uk>
2020-06-14 21:34:24 +0100

    Initialize particles with GPS data

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,6 +20,9 @@
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
+
+vector<Particle> particles;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,7 +33,38 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 500;  // TODO: Set the number of particles
+  
+  // create ramdom number generator engine
+  std::default_random_engine gen;
+
+ 
+  
+ 
+   for(int =0; i<num_particles; i++){
+
+      // create gaussian distribution for x, y and theta
+      normal_distribution<double> dist_x(x,std[0]);
+      normal_distribution<double> dist_y(y, std[1];
+      normal_distribution<double> dist_theta(theta,std[2]);
+    
+     // sample from gaussian distribution
+     double sample_x = dist_x(gen);
+     double sample_y = dist_x(gen);
+     double sample_theta = dist_x(gen);
+    
+     // intialize particle from gaussian distribution 
+     Particle particle;
+     particle.x = sample_x;
+     particle.y = sample_y;
+     particle.theta = sample_theta;
+    
+    // Append each particle to the list of particles
+    particles.append(particle);
+  
+    is_initialized = true;
+  {
+  
 
 }
 
